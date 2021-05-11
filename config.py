35a5758fd369af385a99ed60e71d9b460b6ec2c3
Nikita Play <>
2021-05-11 15:30:26 +0000

    добавили файлы app.p, config.py, handlers.py. Прописали параметры ввода в config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = 1753267381:AAEQCj-tRWpzAppc53q5ptQQeTYfwViOUIk
+admin_id = 16201451311620145131
+todo = {}
+HELP = """
+add - добавить задачи
+show - показать задачи
+done - убрать выпосленную задачу
+help - вывести список команд
+"""
